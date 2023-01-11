e0b17b227b48ff5f22123800e6f08d1dbc4cf720
Tudor Eugeniu <geani.tudor.1997@gmail.com>
2023-01-11 17:32:42 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {
+  Header,
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+} from './containers';
+
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
